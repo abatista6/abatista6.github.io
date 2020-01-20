cb221b6d91ff78c30a06fc96dab87fa91b965eba
Adrianna Batista <adrianna.batista12@gmail.com>
2020-01-20 18:21:16 -0500

    Partition code

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("work-skills").innerHTML = 5 + 6;
+
+//function to toggle dark mode of resume
+function myFunction() {
+   	var element = document.body;
+	element.classList.toggle("dark-mode");
+}

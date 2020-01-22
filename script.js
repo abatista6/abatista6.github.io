3a42bde99711fd5e6ba075e797e177b2a3cdcbb3
Adrianna Batista <adrianna.batista12@gmail.com>
2020-01-22 13:07:37 -0500

    add sticky navbar

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 document.getElementById("work-skills").innerHTML = 5 + 6;
 
-//function to toggle dark mode of resume
+/* function to toggle dark mode of resume */
 function myFunction() {
    	var element = document.body;
 	element.classList.toggle("dark-mode");
+}
+
+/* function for creating sticky navbar */
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
